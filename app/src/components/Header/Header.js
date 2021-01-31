450a9203339addcabcb4da34df251e05c866de34
Konstantin Horkovenko <horkovenko.k@gmail.com>
2021-01-31 18:24:29 +0200

    Completed Header

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import {makeStyles} from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {CenterFocusWeakRounded} from "@material-ui/icons";
+import {Link} from "react-router-dom";
+import {MenuItem} from "@material-ui/core"
+import { useAuth0 } from "@auth0/auth0-react";
+import Avatar from '@material-ui/core/Avatar';
+
+
 
 const useStyles = makeStyles((theme) => ({
-    header: {
-        margin: '0',
-        backgroundColor: 'red',
-        height: '75px'
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        marginLeft: "10px"
+    },
+    logo: {
+        borderRadius: "25px",
+        marginRight: "10px"
     }
 }));
 
-const Header = () => {
+export default function ButtonAppBar() {
     const classes = useStyles();
 
-    return (
-        <Grid item={true} xs={12} lg={12} className={classes.header}>
-            
-        </Grid>
-    );
-};
+    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
-export default Header;
+    const loginSwitch = () => {
+        isAuthenticated ? logout() : loginWithRedirect();
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+
+                    <MenuItem className={classes.logo} component={Link} to={'/'}><CenterFocusWeakRounded
+                        fontSize="large"/>
+                    </MenuItem>
+                    <Avatar src={`${isAuthenticated ? user.picture : ""}`} />
+                    <Typography variant="h6" className={classes.title}>
+                        {`${isAuthenticated ? user.nickname : "Please, Log In!"}`}
+                    </Typography>
+                    <Button color="inherit" onClick={loginSwitch} >{`${isAuthenticated ? "Log Out" : "Log In"}`}</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
--- app/src/components/Profile/Profile.js
+++ app/src/components/Profile/Profile.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+    profile: {
+        overflow: 'hidden'
+    }
+}));
 const Profile = () => {
+    const classes = useStyles();
     const { user } = useAuth0();
 
     return (
-        <div>
+        <div className={classes.profile}>
             {JSON.stringify(user, null, 2)}
         </div>
     );
